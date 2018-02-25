1ffa9c23b86be92643ea66eaa145f770c67aff89
Adam Kotas <adamko034@outlook.com>
2018-02-25 22:51:49 +0100

    Youtube api

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDom from 'react-dom'
 // create a new component, produce HTML
 
+const API_KEY = 'AIzaSyDu9PFL8M4245aL6xe4AhNGAHHZvIQlBms';
+
 const App = () => {
     return <div>Hi!</div>;
 }
+
 
 // put into html dom
 ReactDom.render(<App />, document.querySelector('.container'));
