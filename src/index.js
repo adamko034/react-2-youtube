fc349616e9ff45974cce3d31eb9c5f01839496f7
Adam Kotas <akotas@future-processing.com>
2018-02-27 18:22:28 +0100

    search bar 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-// create a new component, produce HTML
+import React from 'react';
+import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar.js';
 
+// create a new component, produce HTML
 const API_KEY = 'AIzaSyDu9PFL8M4245aL6xe4AhNGAHHZvIQlBms';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 
