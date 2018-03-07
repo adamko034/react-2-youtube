3bc0416b9e12df80dcd2c3bdf40d77f50e254cd5
Adam Kotas <akotas@future-processing.com>
2018-03-07 08:36:24 +0100

    YouTube search 1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar.js';
+import YTSearch from 'youtube-api-search'
 
-// create a new component, produce HTML
 const API_KEY = 'AIzaSyDu9PFL8M4245aL6xe4AhNGAHHZvIQlBms';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: []};
+
+        YTSearch({key: API_KEY, term: 'snowboard'}, videos => {
+            this.setState({videos})
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 
-// put into html dom
 ReactDom.render(<App />, document.querySelector('.container'));
